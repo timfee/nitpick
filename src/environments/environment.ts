9d19b9aced92b77c4676be23bf06dc53f0668aec
Tim Feeley <feel@google.com>
2026-07-12 18:26:59 +0000

    Set OAuth client ID in environment resource sets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   project: 'nitpick-6an5rp',
   region: 'us-central1',
   /** OAuth web client ID for Sign in with Google. */
-  googleClientId: '',
+  googleClientId: '204060019607-fl1ehebdji768hn3t7o2h0ngmalp4ha4.apps.googleusercontent.com',
   geminiModel: 'gemini-2.5-flash',
   vertexLocation: 'global',
 };
